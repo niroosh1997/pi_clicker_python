dbe95b884a7ca83c47d556229c0c25f58252b7d3
niroosh1997 <niroosh1997@gmail.com>
2021-08-27 21:23:56 +0300

    Finished changing protocol and getting angle from phone

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from concurrent import futures
 import logging
 
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
+import changeangle_pb2
+import changeangle_pb2_grpc
 
+class ChangeAngle(changeangle_pb2_grpc.ChangeAngleServiceServicer):
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
-
-    def SayHello(self, request, context):
-        print("yoyoyoyoo")
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+    def changeAngle(self, request, context):
+        print(request.angle)
+        print(f"host is: {context.peer()}")
+        # todo: add change angle to servo
+        return changeangle_pb2.ChangeAngleReply(message='Change angle accepted')
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('localhost:50051')
+    changeangle_pb2_grpc.add_ChangeAngleServiceServicer_to_server(ChangeAngle(), server)
+    server.add_insecure_port('10.100.102.13:50051')
     server.start()
     print("waiting for termination")
     server.wait_for_termination()
